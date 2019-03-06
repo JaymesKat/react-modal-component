e28956b087ece4da933f853f08168cb4b7f9ce83
James Katarikawe <james.katarikawe@andela.com>
2019-03-06 16:56:54 +0300

    Updates

---
+++ precache-manifest.cf748a73884e0078b25e8f2380e7dab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2539c00854e4514937ee",
+    "url": "/JaymesKat/redux-modal-component/static/css/main.9c07a35f.chunk.css"
+  },
+  {
+    "revision": "2539c00854e4514937ee",
+    "url": "/JaymesKat/redux-modal-component/static/js/main.2539c008.chunk.js"
+  },
+  {
+    "revision": "d12a2b191f2a9508e0cf",
+    "url": "/JaymesKat/redux-modal-component/static/js/runtime~main.d12a2b19.js"
+  },
+  {
+    "revision": "53f0fd9fd0ae6c42e45a",
+    "url": "/JaymesKat/redux-modal-component/static/js/2.53f0fd9f.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JaymesKat/redux-modal-component/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a986ac4f548a0ba283a0cc8066673d8f",
+    "url": "/JaymesKat/redux-modal-component/index.html"
+  }
+];
