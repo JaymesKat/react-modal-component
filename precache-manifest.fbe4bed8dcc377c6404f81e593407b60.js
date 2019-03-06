c3de36471824fcff1dfde17a6d9d3a1f9a9bc338
James Katarikawe <james.katarikawe@andela.com>
2019-03-06 17:03:04 +0300

    Updates

---
+++ precache-manifest.fbe4bed8dcc377c6404f81e593407b60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "60123e4d437f9a08a8b5",
+    "url": "/redux-modal-component/static/css/main.9c07a35f.chunk.css"
+  },
+  {
+    "revision": "60123e4d437f9a08a8b5",
+    "url": "/redux-modal-component/static/js/main.60123e4d.chunk.js"
+  },
+  {
+    "revision": "4907197bf0609ad0ccd0",
+    "url": "/redux-modal-component/static/js/runtime~main.4907197b.js"
+  },
+  {
+    "revision": "53f0fd9fd0ae6c42e45a",
+    "url": "/redux-modal-component/static/js/2.53f0fd9f.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/redux-modal-component/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5a0c83147e8e06e3861a6a271f7e5c61",
+    "url": "/redux-modal-component/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/JaymesKat/redux-modal-component/precache-manifest.cf748a73884e0078b25e8f2380e7dab4.js"
+  "/redux-modal-component/precache-manifest.fbe4bed8dcc377c6404f81e593407b60.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/JaymesKat/redux-modal-component/index.html", {
+workbox.routing.registerNavigationRoute("/redux-modal-component/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
